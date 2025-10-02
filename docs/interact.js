f3dc4fa23313620047b41899daa9ed7b629be50b
WASHBASlN <sls7364@psu.edu>
2025-10-02 15:17:32 -0400

    js

---
+++ docs/interact.js
@@ -0,0 +1,23 @@
+function changeColor() {
+var colorMe1 = document.getElementById("colorToggle") 
+colorMe1.style.backgroundColor = "skyblue";}
+
+/* here, style is a *property*: the CSS styling of an element: you can add a CSS property after invoking style. */
+
+
+function display() {
+    var opt = document.getElementById("opt");
+    
+}
+
+
+function toggleImage() {
+
+var toggle = document.getElementById("monkey");
+  if (toggle.style.display === "none") {
+    toggle.style.display = "block";
+  } else {
+    toggle.style.display = "none";
+  }
+}
+
